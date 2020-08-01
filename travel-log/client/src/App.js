15cfb644dcce807a2818d5e1806687942c2e96f7
SujeethJinesh <sujeethjinesh@gmail.com>
2020-08-01 04:39:14 -0700

    Finish Project

--- travel-log/client/src/App.js
+++ travel-log/client/src/App.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import ReactMapGL, { Marker, Popup } from 'react-map-gl';
+import { ReactComponent as MapPinRed } from './icons/map-pin-red.svg';
+import { ReactComponent as MapPinYellow } from './icons/map-pin-yellow.svg';
 
 import { listLogEntries } from './API';
+import LogEntryForm from './LogEntryForm';
 
 const App = () => {
   const [logEntries, setLogEntries] = useState([]);
   const [showPopup, setShowPopup] = useState([]);
+  const [addEntryLocation, setAddEntryLocation] = useState(null);
   const [viewport, setViewport] = useState({
     width: '100vw',
     height: '100vh',
@@ -14,24 +18,33 @@ const App = () => {
     zoom: 3,
   });
 
-  // hook to make a request to our backend
-  // if we see a cors issue, it means that on the backend we need to allow
-  // http calls from the correct port, ie 3000 in this instance
-  useEffect(() => {
+  const getEntries = async () => {
     // since we can't make async requests in useEffect
     // (cause of race conditions), we need to use iife
     // iife is immediately invoked function expression
 
     // essentially this is an async function that is being run immediately
-    (async () => {
-      const logEntries = await listLogEntries();
-      setLogEntries(logEntries);
-      console.log(logEntries);
-    })();
+    const logEntries = await listLogEntries();
+    setLogEntries(logEntries);
+    console.log(logEntries);
+  };
+
+  // hook to make a request to our backend
+  // if we see a cors issue, it means that on the backend we need to allow
+  // http calls from the correct port, ie 3000 in this instance
+  useEffect(() => {
+    getEntries();
   }, []); // throw any dependencies in here, so technically setLogEntries
 
   const showAddMarkerPopup = (event) => {
     console.log(event);
+
+    const [longitude, latitude] = event.lngLat;
+
+    setAddEntryLocation({
+      latitude,
+      longitude,
+    });
   };
 
   return (
@@ -46,12 +59,8 @@ const App = () => {
         // if you want to just return jsx,
         // use () without return instead of {} with return
         return (
-          <>
-            <Marker
-              key={entry._id}
-              latitude={entry.latitude}
-              longitude={entry.longitude}
-            >
+          <React.Fragment key={entry._id}>
+            <Marker latitude={entry.latitude} longitude={entry.longitude}>
               <div
                 onClick={() =>
                   setShowPopup({
@@ -59,15 +68,14 @@ const App = () => {
                   })
                 }
               >
-                <img
+                <MapPinYellow
                   className='marker'
                   style={{
                     height: `${6 * viewport.zoom}px`,
                     width: `${6 * viewport.zoom}px`,
                   }}
-                  src='https://i.imgur.com/y0G5YTX.png'
                   alt='marker'
-                ></img>
+                />
               </div>
             </Marker>
             {showPopup[entry._id] ? (
@@ -86,12 +94,53 @@ const App = () => {
                   <small>
                     Visited on: {new Date(entry.visitDate).toLocaleDateString()}
                   </small>
+                  {!!entry.image ? (
+                    <img src={entry.image} alt={entry.title}></img>
+                  ) : null}
                 </div>
               </Popup>
             ) : null}
-          </>
+          </React.Fragment>
         );
       })}
+      {addEntryLocation ? (
+        <>
+          <Marker
+            latitude={addEntryLocation.latitude}
+            longitude={addEntryLocation.longitude}
+          >
+            <div>
+              <MapPinRed
+                className='marker'
+                style={{
+                  height: `${6 * viewport.zoom}px`,
+                  width: `${6 * viewport.zoom}px`,
+                }}
+                alt='marker'
+              />
+            </div>
+          </Marker>
+          <Popup
+            latitude={addEntryLocation.latitude}
+            longitude={addEntryLocation.longitude}
+            closeButton={true}
+            closeOnClick={false}
+            dynamicPosition={true}
+            onClose={() => setAddEntryLocation(null)}
+            anchor='top'
+          >
+            <div className='popup'>
+              <LogEntryForm
+                location={addEntryLocation}
+                onClose={() => {
+                  setAddEntryLocation(null);
+                  getEntries();
+                }}
+              />
+            </div>
+          </Popup>
+        </>
+      ) : null}
     </ReactMapGL>
   );
 };
